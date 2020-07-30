c311aa45aa29c951882d97e7e99e8a6788cd44e7
Federico N <nfederico@live.com>
2020-07-30 20:32:21 -0300

    All test passed but CSV import functionality

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,19 @@
-// import AppError from '../errors/AppError';
+ import AppError from '../errors/AppError';
+ import Transaction from '../models/Transaction';
+ import TransactionsRepository from '../repositories/TransactionsRepository';
+import { getCustomRepository } from 'typeorm';
+import { response } from 'express';
+
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id :string): Promise<void> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+    const transaction = await transactionRepository.findOne(id);
+    if (! transaction) {
+      throw new AppError('Transaction do not exists');
+    }
+    await transactionRepository.remove(transaction);
+
   }
 }
 
